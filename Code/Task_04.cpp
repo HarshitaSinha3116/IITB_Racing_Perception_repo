b38164acc3310a6766525925e56f54dafb9a64f9
HarshitaSinha3116 <225584548+HarshitaSinha3116@users.noreply.github.com>
2026-03-27 22:07:48 +0530

    Add includes for point cloud and visualization messages
    Added necessary includes for point cloud processing

--- Code/Task_04.cpp
+++ Code/Task_04.cpp
@@ -1,1 +1,13 @@
+#include<rclcpp/rclcpp.hpp>
+#include<sensor_msgs/msg/point_cloud.hpp>
+#include<visualization_msgs/msg/marker_array.hpp>
+#include<geometry_msgs/msg/point32.hpp>
+#include<sensor_msgs/msg/channel_float32.hpp>
+
+#include<vector>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+
+
 
